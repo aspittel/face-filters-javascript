abd4653d3b1af31425a57115e635ed5dd0b57bd3
aspittel <ali07cat07@gmail.com>
2017-12-05 20:57:45 -0500

    adding filters and css

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
 const tracker = new tracking.ObjectTracker('face')
-const download = document.getElementById('download')
+const flowerCrownButton = document.getElementById('flower-crown')
+const bunnyEarsButton = document.getElementById('bunny-ears')
 
-const flowerCrown = new Image()
-flowerCrown.src = 'flower-crown.png'
+const img = new Image()
+let filterX = 0
+let filterY = 0
+let filterWidth = 0
+let filterHeight = 0
+
+function changePic (x, y, width, height, src) {
+  img.src = src
+  filterX = x
+  filterY = y
+  filterWidth = width
+  filterHeight = height
+}
+
+function flowerCrown () {
+  changePic(0, -0.5, 1, 1, 'flower-crown.png')
+}
+
+flowerCrown()
+
+flowerCrownButton.addEventListener('click', flowerCrown)
+
+bunnyEarsButton.addEventListener('click', () => {
+  changePic(-0.5, -0.9, 2, 2, 'bunny-ears.png')
+})
 
 tracker.setInitialScale(4)
 tracker.setStepSize(2)
@@ -14,12 +38,10 @@ tracking.track('#video', tracker, { camera: true })
 tracker.on('track', event => {
   context.clearRect(0, 0, canvas.width, canvas.height)
   event.data.forEach(rect => {
-    context.drawImage(flowerCrown, rect.x, rect.y - 70, rect.width + 5, rect.height)
+    context.drawImage(img, rect.x + (filterX * rect.width),
+    rect.y + (filterY * rect.height),
+    rect.width * filterWidth,
+    rect.height * filterHeight
+  )
   })
-})
-
-download.addEventListener('click', function () {
-  console.log(this, 'hello world 2')
-  this.href = canvas.toDataURL()
-  this.download = 'test.png'
 })
